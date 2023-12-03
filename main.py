080fa3edc024360cddf0ac2a47c41fc86738c8c1
besplago <bruce_esplago@hotmail.com>
2023-12-03 11:55:35 +0100

    more features

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+OUTPUT_PATH = '.'
 URL = 'https://www.nybolig.dk'
 PAGES = 1
 MAX_PAGES = 2404
@@ -24,11 +25,13 @@ def _extract_bolig_data(bolig_url: str) -> None:
     source: str = requests.get(bolig_url, headers={'User-Agent': 'Mozilla/5.0'}).text
     soup: BeautifulSoup = BeautifulSoup(source, HTML_PARSER)
     bolig_data: dict = {}
+    bolig_data['url'] = bolig_url
     bolig_data['address'] = _extract_address(soup)
-    bolig_data['price'] = soup.find('span', class_='case-info__property__info__text__price').text.strip()
+    bolig_data['price'] = _extract_price(soup)
     bolig_data['size'] = _extract_size(soup)
-
-    print(bolig_data)
+    bolig_data['rooms'] = _extract_rooms(soup)
+    bolig_data['year_built'] = _extract_year_built(soup)
+    bolig_data['year_renovated'] = _extract_year_renovated(soup)
 
 def _start_scraping(pages: int) -> None:
     pages: int = _get_pages(pages)
@@ -55,10 +58,39 @@ def _start_scraping(pages: int) -> None:
         page += 1
     print(f"Finished scraping {pages} pages")
 
-def _extract_size(soup: BeautifulSoup) -> str:
-    for fact in soup.find_all('div', class_='case-facts__box'):
+def _extract_year_renovated(soup: BeautifulSoup) -> int:
+    for fact in soup.find_all('div', class_='case-facts__box-inner-wrap'):
+        if 'Bygget/Ombygget' in fact.text:
+            year_renovated_raw: str = fact.find('strong').text.split('/')
+            if len(year_renovated_raw) > 1:
+                year_renovated: int = int(year_renovated_raw[1])
+                return year_renovated
+            else:
+                return None
+
+def _extract_year_built(soup: BeautifulSoup) -> int:
+    for fact in soup.find_all('div', class_='case-facts__box-inner-wrap'):
+        if 'Bygget/Ombygget' in fact.text:
+            year_built: int = int(fact.find('strong').text.split('/')[0])
+            return year_built
+
+def _extract_rooms(soup: BeautifulSoup) -> int:
+    for fact in soup.find_all('div', class_='case-facts__box-inner-wrap'):
+        if 'Stue/Værelser' in fact.text:
+            living_rooms: int = int(fact.find('strong').text.split('/')[0])
+            rooms: int = int(fact.find('strong').text.split('/')[1])
+            return living_rooms + rooms
+
+def _extract_size(soup: BeautifulSoup) -> int:
+    for fact in soup.find_all('div', class_='case-facts__box-inner-wrap'):
         if 'Boligareal' in fact.text:
-            return fact.find('strong').text.strip()
+            return int(fact.find('strong').text.split(' ')[0])
+
+def _extract_price(soup: BeautifulSoup) -> int:
+    price_raw: str = soup.find('span', class_='case-info__property__info__text__price').text.strip()
+    # remove non-numeric characters
+    price: int = int(''.join(filter(str.isdigit, price_raw)))
+    return price
 
 def _extract_address(soup: BeautifulSoup) -> str:
     # Extract the address components and join them with a space
